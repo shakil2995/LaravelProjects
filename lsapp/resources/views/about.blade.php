db0c362a0f7e8b27688487f9daabcc26a171746a
Shakil <shakil2995@gmail.com>
2022-04-16 19:02:04 +0600

    blade templating

--- lsapp/resources/views/about.blade.php
+++ lsapp/resources/views/about.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lsapp</title>
-</head>
-<body>
-    <h1>About Me</h1>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <h1>About the app</h1>
+@endsection
--- lsapp/resources/views/index.blade.php
+++ lsapp/resources/views/index.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lsapp</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
     <h1>Welcome to my webapp</h1>
-</body>
-</html>
+@endsection
