faf8f9422610bc639fba46cdda8d5cc9df970d36
Shakil <shakil2995@gmail.com>
2022-04-18 21:59:53 +0600

    ui

--- lsapp/resources/views/layouts/app.blade.php
+++ lsapp/resources/views/layouts/app.blade.php
@@ -4,17 +4,18 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Lsapp</title>
-  {{-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" /> --}}
-  <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}" />
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  {{-- <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}" /> --}}
   <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}" />
   <link href=./styles/main.css rel="stylesheet" type="text/css">
 </head>
 <body>
-    <main>
+    {{-- <main> --}}
+      @include('inc.navbar')
       <div class="container">
         @yield('content')
       </div>
-    </main>
+    {{-- </main> --}}
     <footer>
     </footer>       
 </body>
