96b3ca3877c788e725ae1d7f8f75fa7e0522d892
Shakil <shakil2995@gmail.com>
2022-04-21 19:18:32 +0600

    Update Post.php

--- lsapp/app/Models/Post.php
+++ lsapp/app/Models/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
-    use HasFactory;
+{   
+    // table name
+    protected $table = 'posts';
+    public $primaryKey = 'id';
+
 }
