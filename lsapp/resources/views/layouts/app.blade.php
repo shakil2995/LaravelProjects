0b06886ad7b7f3d35542192461b38def5132d4b0
Shakil <shakil2995@gmail.com>
2022-04-22 00:22:19 +0600

    fixed js bug

--- lsapp/resources/views/layouts/app.blade.php
+++ lsapp/resources/views/layouts/app.blade.php
@@ -1,27 +1,14 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> --}}
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <title>Lsapp</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}" />
-    {{-- <link href=./styles/main.css rel="stylesheet" type="text/css"> --}}
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>Lsapp</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.min.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}" />
 </head>
 <body>
     <div id="app">
@@ -29,9 +16,17 @@
         <div class="container">
             @include('inc.messages')
             @yield('content')
-        <main class="py-4">
-            {{-- @yield('content') --}}
-        </main>
     </div>
+    <footer>
+    </footer> 
+    <script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
+      <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>      
 </body>
+<script src="https://code.jquery.com/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </html>
