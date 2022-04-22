1f80a65a0e70e5ba0f6393613bea15d5080ab5d9
Shakil <shakil2995@gmail.com>
2022-04-22 15:57:01 +0600

    updated

--- lsapp/resources/views/pages/about.blade.php
+++ lsapp/resources/views/pages/about.blade.php
@@ -1,4 +1,35 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{$title}}</h1>
+    <h1>{{$title}} The Project</h1>
+    <p>
+        Hi this is a group project for CSE309 Final.
+    </p>
+    <h4>Group members :</h4>
+    <ul>
+        
+        <li>Shayla Sharmin Akhi</li>
+        <li>Md Shakil Ahmed</li>
+    </ul>
+    <div class="container card-group">
+        <div class="card md-3 m-3" style="max-width: 30rem;">
+            <div class="card-body">
+              {{-- <h5 class="card-title">Special title treatment</h5> --}}
+              {{-- <h6 class="card-subtitle text-muted">Support card subtitle</h6> --}}
+            </div>
+                <img src="{{ url('images/1.jpg') }}" alt="">
+                <h3 class="card-header">Shayla Sharmin Akhi</h3>
+        </div>
+        <div class="card md-3 m-3" style="max-width: 30rem;">
+            
+            <div class="card-body">
+              {{-- <h5 class="card-title">Special title treatment</h5> --}}
+              {{-- <h6 class="card-subtitle text-muted">Support card subtitle</h6> --}}
+            </div>
+                <img src="{{ url('images/1.jpg') }}" alt="">
+                <h3 class="card-header">Md Shakil Ahmed</h3>
+                {{-- id:1730295 --}}
+        </div>
+
+    </div>
+    
 @endsection
